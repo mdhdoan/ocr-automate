ff25cfebd5daa072a38d8adbfae3d0637ac7d3dc
Minh Doan <minh.doan@dfo-mpo.gc.ca>
2025-05-28 15:41:48 -0700

    update test suits

--- python/ocr.py
+++ python/ocr.py
@@ -1,7 +1,10 @@
 ##### LIBRARIES #####
 from datetime import datetime
+from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain.output_parsers.pydantic import PydanticOutputParser
+from langchain_ollama import ChatOllama
 from langchain_ollama import OllamaLLM
 from io import BytesIO
 from PIL import Image
@@ -9,7 +12,6 @@ from pydantic import BaseModel, Field, model_validator
 
 import base64
 import os 
-import pytesseract
 # import shutil
 import sys
 
@@ -47,6 +49,7 @@ llm = OllamaLLM(model = model_select, temperature = 0.0)
 prompt = create_prompt(format_instructions)
 llm_chain = prompt | llm | output_parser
 
+vision_model_list = ["minicpm-v","granite3.2-vision","gemma3", "qwen2.5vl", "llama3.2-vision"]
 
 ##### FUNCTIONS #####
 def convert_to_base64(pil_image):
@@ -83,28 +86,60 @@ def intake_pdf_from_dir(directory):
 
 def intake_img_from_dir(directory):
     loaded_list_of_img_files = {}
+
     # Load the PDF file using pypdf library
     for img in directory:
         uuid = img[-10:-4]
         print(f"\tuuid: {uuid}")
         file_address = os.path.join(sys.argv[1], img)
-        # image = Image.open(file_address)
-        with open(file_address, 'rt', encoding='utf-8') as pdf_file:
-            loaded_list_of_img_files[uuid] = pdf_file
+        pil_image = Image.open(file_address)
+        image_b64 = convert_to_base64(pil_image)
         loaded_list_of_img_files[uuid] = file_address
     return loaded_list_of_img_files
+
+def prompt_func(data):
+    text = data["text"]
+    image = data["image"]
+    image_part = {
+        "type": "image_url",
+        "image_url": f"data:image/jpeg;base64,{image}",
+    }
+    content_parts = []
+    text_part = {"type": "text", "text": text}
+
+    content_parts.append(image_part)
+    content_parts.append(text_part)
+
+    return [HumanMessage(content=content_parts)]
+
+def testing_visual_models(img):
+    file_path = img
+    pil_image = Image.open(file_path)
+
+    image_b64 = convert_to_base64(pil_image)
+    for vision_model in vision_model_list:
+        attempt = 1
+        while attempt < 10:
+            llm = ChatOllama(model=vision_model, temperature=0)
+            chain = prompt_func | llm | StrOutputParser()
+            query_chain = chain.invoke(
+                {"text": "This form has a COMMENTS section to the bottom right. Identify the COMMENTS data and provide me only the comments. If it's not in english, provide me both the raw and what you could improve to make it meaningful.", "image": image_b64}
+            )
+            print("MODEL:", vision_model, "\n\tAttempt:", attempt,"\n\tANSWER:", query_chain)
+            attempt += 1
 
 
 ###---------------------------------------------------------------###
 if __name__ == "__main__": 
     start_time = datetime.now()
     print("Running local at", start_time)
-    dir_of_jsons = intake_img_from_dir(file_list_in_directory)
-    print(f"Finished {len(dir_of_jsons)} files")
-    for doc_id, doc in dir_of_jsons.items():
-        extracted_header = llm_extract(doc)
+    dir_of_imgs = intake_img_from_dir(file_list_in_directory)
+    print(f"Finished {len(dir_of_imgs)} files")
+    for doc_id, doc in dir_of_imgs.items():
+        # extracted_header = llm_extract(doc)
+        print(f"-----#####{doc_id}#####-----\n{testing_visual_models(doc)}")
         # print(f"For ID {doc_id}, the content is:\n\t{doc}\n\tHeader is: {extracted_header}")
-        print(f"For ID {doc_id}, the content is:\n\t{doc}\n\t")
+        # print(f"For ID {doc_id}, the content is:\n\t{doc}\n\t")
 
     end_time = datetime.now()
     seconds = (end_time - start_time).total_seconds()
--- python/test.py
+++ python/test.py
@@ -41,41 +41,41 @@ from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 
 # llama_chain = ["minicpm-v","llava","bakllava","llava-llama3","moondream","llava-phi3","granite3.2-vision","gemma3", "llama4", "qwen2.5vl", "llama3.2-vision"]
-llama_chain = ["minicpm-v","granite3.2-vision","gemma3", "qwen2.5vl", "llama3.2-vision"]
+# llama_chain = ["minicpm-v","granite3.2-vision","gemma3", "qwen2.5vl", "llama3.2-vision"]
 # llm = ChatOllama(model="minicpm-v", temperature=0)
-# llm = ChatOllama(model="moondream", temperature=0)
-attempt = 1
-for vision_model in llama_chain:
+llm = ChatOllama(model="llama3.2-vision", temperature=0)
+# attempt = 1
+# for vision_model in llama_chain:
 
-    while attempt < 10:
-        llm = ChatOllama(model=vision_model, temperature=0)
+# while attempt < 10:
+    # llm = ChatOllama(model=vision_model, temperature=0)
 
-        def prompt_func(data):
-            text = data["text"]
-            image = data["image"]
+def prompt_func(data):
+    text = data["text"]
+    image = data["image"]
 
-            image_part = {
-                "type": "image_url",
-                "image_url": f"data:image/jpeg;base64,{image}",
-            }
+    image_part = {
+        "type": "image_url",
+        "image_url": f"data:image/jpeg;base64,{image}",
+    }
 
-            content_parts = []
+    content_parts = []
 
-            text_part = {"type": "text", "text": text}
+    text_part = {"type": "text", "text": text}
 
-            content_parts.append(image_part)
-            content_parts.append(text_part)
+    content_parts.append(image_part)
+    content_parts.append(text_part)
 
-            return [HumanMessage(content=content_parts)]
+    return [HumanMessage(content=content_parts)]
 
 
-        from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 
-        chain = prompt_func | llm | StrOutputParser()
+chain = prompt_func | llm | StrOutputParser()
 
-        query_chain = chain.invoke(
-            {"text": "What is the handwritten message and what is the table's title?", "image": image_b64}
-        )
+query_chain = chain.invoke(
+    {"text": "What is the handwritten message and what is the table's title?", "image": image_b64}
+)
 
-        print("MODEL:", vision_model, "\n\tAttempt:", attempt,"\n\tANSWER:", query_chain)
-        attempt += 1
+print("ANSWER:", query_chain)
+# attempt += 1

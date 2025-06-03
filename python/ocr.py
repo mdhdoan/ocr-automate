58cfefc77a0853e9b1f52e7ca993e2a18e2d7aa9
Minh Doan <minh.doan@dfo-mpo.gc.ca>
2025-06-03 13:49:36 -0700

    change reasoning model

--- python/ocr.py
+++ python/ocr.py
@@ -78,7 +78,7 @@ output_parser = JsonOutputParser()
 format_instructions = output_parser.get_format_instructions()
 vision_model_list = ["qwen2.5vl"]
 # vision_model_list = ["qwen2.5vl", "llama3.2-vision"]
-reasoning_model_list =["gemma3"]
+reasoning_model_list =["llama3.1"]
 ##### FUNCTIONS #####
 def convert_to_base64(pil_image):
     """
